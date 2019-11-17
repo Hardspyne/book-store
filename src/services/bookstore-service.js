042eec9f409ab3f1ab74c9a505679989d46c6d69
Hardspyne <79159707922@yandex.ru>
2019-11-17 12:16:27 +0300

    add pages and routing

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,6 +1,6 @@
 export default class BookStoreService {
 
     getBooks() {
-        return [];
+        return [{author: 'author1', title: 'title1'}, {author: 'author2', title: 'title2'}];
     }
 }
