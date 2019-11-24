a3bbfbc3746e341c74436a40d4011163ec9e17a5
Hardspyne <79159707922@yandex.ru>
2019-11-24 17:23:42 +0300

    Updates

---
+++ precache-manifest.fffbc6a0c5b7f7c6b683b5b983098d6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae06cc6956c0d196a92e46b2ce2ade36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ebfb5b95ec1bf061c15",
+    "url": "/static/css/main.76c22534.chunk.css"
+  },
+  {
+    "revision": "d7318d3bd9da3cf5fff2",
+    "url": "/static/js/2.378d1534.chunk.js"
+  },
+  {
+    "revision": "1ebfb5b95ec1bf061c15",
+    "url": "/static/js/main.1fb7e9f4.chunk.js"
+  },
+  {
+    "revision": "92bf8e40333aaa2266c5",
+    "url": "/static/js/runtime-main.32d57ab8.js"
+  }
+]);
