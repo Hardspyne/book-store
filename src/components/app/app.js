190df6f43d218da8a67c005620c34d3225b48d94
Hardspyne <79159707922@yandex.ru>
2019-11-24 17:34:13 +0300

    add redirect

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './app.css';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import {BookPage, CartPage, HomePage} from '../pages';
 import Header from "../header";
 
@@ -12,6 +12,7 @@ const App = () => {
                 <Route path='/' component={HomePage} exact/>
                 <Route path='/cart' component={CartPage}/>
                 <Route path='/books/:id' component={BookPage}/>
+                <Redirect to='/'/>
             </Switch>
         </main>
     )
